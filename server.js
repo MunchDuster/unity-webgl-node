aa50b337eac9c1d61bbc6f46efc27513469bd831
MunchDuster <72060614+MunchDuster@users.noreply.github.com>
2021-07-22 11:29:51 +1200

    fix opening on wrong port

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var server = require('http').createServer(app);
-var cors = require('cors')
+const PORT = process.env.PORT || 8080;
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const cors = require('cors')
 app.use(cors())// Set cross-domain
 app.use(express.static('client'));// Static file folder
 // Listening port number
-server.listen(3005, function () {
+server.listen(PORT, function () {
     console.log('App listening at port 3005;');
 });
